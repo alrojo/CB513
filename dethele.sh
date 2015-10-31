12063c56b05df977584cb43cf978da11c805df92
A. Rosenberg Johansen <alexander@herhjemme.dk>
2015-10-31 12:52:19 +0800

    batch script

---
+++ dethele.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+python train.py T1
+python train.py T2
+python train.py T3
+python train.py T4
+python train.py T5
+python train.py T6
+python train.py T7
+python train.py T8
+python train.py T9
+python train.py T10

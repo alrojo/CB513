2281019de31d1ad5af74e0ab72cd2e5175e6da01
Alexander Rosenberg Johansen <s145706@mnemosyne.imm.dtu.dk>
2015-12-19 11:01:00 +0100

    debugging batchnormlayer

--- configurations/T33.py
+++ configurations/T33.py
@@ -1,7 +1,5 @@
 import lasagne
 import numpy as np
-import BatchNormLayer
-batchnorm = BatchNormLayer.batch_norm
 # 3 conv before FC, 400 LSTM
 
 #validate_every = 40
@@ -26,7 +24,7 @@ n_inputs = 42
 num_classes = 8
 seq_len = 700
 optimizer = "rmsprop"
-lambda_reg = 0.0003 
+lambda_reg = 0.0001 
 cut_grad = 20
 
 learning_rate_schedule = {
@@ -41,26 +39,18 @@ learning_rate_schedule = {
 def build_model():
     # 1. Input layer
     l_in = lasagne.layers.InputLayer(shape=(None, seq_len, n_inputs))
-
     l_dim_a = lasagne.layers.DimshuffleLayer(
 	l_in, (0,2,1))
-
     l_conv_a = lasagne.layers.Conv1DLayer(
 	incoming=l_dim_a, num_filters=N_CONV_A, pad='same',
-	filter_size=F_CONV_A, stride=1, nonlinearity=lasagne.nonlinearities.rectify)
-    l_conv_a_b = batch_norm(l_conv_a)
-
+	filter_size=F_CONV_A, stride=1, nonlinearity=lasagne.nonlinearities.very_leaky_rectify)
     l_conv_b = lasagne.layers.Conv1DLayer(
 	incoming=l_dim_a, num_filters=N_CONV_B, pad='same',
-	filter_size=F_CONV_B, stride=1, nonlinearity=lasagne.nonlinearities.rectify)
-    l_conv_b_b = batch_norm(l_conv_b)
-    
+	filter_size=F_CONV_B, stride=1, nonlinearity=lasagne.nonlinearities.very_leaky_rectify)
     l_conv_c = lasagne.layers.Conv1DLayer(
 	incoming=l_dim_a, num_filters=N_CONV_C, pad='same',
-	filter_size=F_CONV_C, stride=1, nonlinearity=lasagne.nonlinearities.rectify)
-    l_conv_c_b = batch_norm(l_conv_c)
-
-    l_c_a = lasagne.layers.ConcatLayer([l_conv_a_b, l_conv_b_b, l_conv_c_b], axis=1)
+	filter_size=F_CONV_C, stride=1, nonlinearity=lasagne.nonlinearities.very_leaky_rectify)
+    l_c_a = lasagne.layers.ConcatLayer([l_conv_a, l_conv_b, l_conv_c], axis=1)
     l_dim_b = lasagne.layers.DimshuffleLayer(
 	l_c_a, (0,2,1))
     l_c_b = lasagne.layers.ConcatLayer([l_in,l_dim_b], axis=2)
@@ -68,9 +58,7 @@ def build_model():
     l_reshape_a = lasagne.layers.ReshapeLayer(
         l_c_b, (batch_size*seq_len,n_inputs+16*3))
     l_1 = lasagne.layers.DenseLayer(
-        l_reshape_a, num_units=N_L1, nonlinearity=lasagne.nonlinearities.rectify)
-    l_1 = batch_norm(l_1)
-
+        l_reshape_a, num_units=N_L1, nonlinearity=lasagne.nonlinearities.very_leaky_rectify)
     l_reshape_b = lasagne.layers.ReshapeLayer(
         l_1, (batch_size, seq_len, N_L1))
 #    batch_size, seq_len, _ = l_in.input_var.shape
@@ -89,8 +77,7 @@ def build_model():
         l_sum, (batch_size*seq_len, N_LSTM_F+N_LSTM_B))
     # Our output layer is a simple dense connection, with 1 output unit
     l_2 = lasagne.layers.DenseLayer(
-	lasagne.layers.dropout(l_reshape_b, p=0.5), num_units=N_L2, nonlinearity=lasagne.nonlinearities.rectify)
-    l_2 = batch_norm(l_2)
+	lasagne.layers.dropout(l_reshape_b, p=0.5), num_units=N_L2, nonlinearity=lasagne.nonlinearities.very_leaky_rectify)
     # 5. Output Layer
     l_recurrent_out = lasagne.layers.DenseLayer(
         l_2, num_units=num_classes, nonlinearity=lasagne.nonlinearities.softmax)
@@ -100,3 +87,9 @@ def build_model():
         l_recurrent_out, (batch_size, seq_len, num_classes))
 
     return l_in, l_out
+
+def set_weights():
+    metadata_path = "metadata/weight_save/dump_T27-20151213-080541-300.pkl"
+    print "setting weights to: %s" % metadata_path
+    metadata = np.load(metadata_path)
+    return metadata['param_values']
